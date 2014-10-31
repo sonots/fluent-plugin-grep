da24e8b7752ccf5f6b41ac1bc9e22d0f7823f12f
sonots <sonots@gmail.com>
2014-10-31 15:37:21 +0900

    fix test for old fluentd version

--- test/helper.rb
+++ test/helper.rb
@@ -6,5 +6,3 @@ unless defined?(Test::Unit::AssertionFailedError)
   class Test::Unit::AssertionFailedError < StandardError
   end
 end
-
-$log = Fluent::Log.new(Fluent::Test::DummyLogDevice.new, Fluent::Log::LEVEL_WARN)
